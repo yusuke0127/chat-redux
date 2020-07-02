ec7d3e2811e749e9eee3f00f4b03a0756f17a066
yusuke ishida <ishidayusuke01@gmail.com>
2020-07-02 16:11:33 +0900

    Added redux promise

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
+import reduxPromise from 'redux-promise';
 
 // internal modules
 import App from './components/app';
@@ -24,7 +25,7 @@ const reducers = combineReducers({
   selectedChannel: selectedChannelReducer
   // changeMe: (state = null, action) => state
 });
-const middlewares = applyMiddleware(logger);
+const middlewares = applyMiddleware(reduxPromise, logger);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
