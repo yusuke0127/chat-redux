a02a279a48449eb299f64af656afe6a740010dce
yusuke ishida <ishidayusuke01@gmail.com>
2020-07-04 12:57:52 +0900

    Fixed bugs for form

--- src/containers/message_form.jsx
+++ src/containers/message_form.jsx
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
-
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
 class MessageForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+  }
+
+  handleChange = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      value: event.target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.currentTarget.value);
+  }
+
   render() {
     return (
-      <form className="message-form">
-        <input type="text" name="message" id="" />
+      <form className="message-form" onSubmit={this.handleSubmit}>
+        <input type="text" name="message" id="" onChange={this.handleChange} value={this.state.value} />
         <button type="submit">Send</button>
       </form>
     );
   }
+}
+
+function mapStateToprops(state) {
+  return {
+
+  };
 }
 
 export default MessageForm;
